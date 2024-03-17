e1d91d0da474a6dee2a5a77bc3883ba06308dc0f
Emma <markhamonca@codeninjas.com>
2024-03-17 14:00:30 -0400

    step 26

---
+++ AY-Sumerian Game/Assets/Scenes/PLayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PLayer : MonoBehaviour
+{
+    public string playerName;
+    private ulong highestPop;
+    private GameplayManager gameManager;
+
+    public TMP_Text nameText;
+    public TMP_Text yearText;
+    public TMP_Text highestPopText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = "Ruler: " + playerName;
+
+        gameManager = GetComponent<GameplayManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yearText.text = "Year: " + gameManager.year;
+    }
+}
